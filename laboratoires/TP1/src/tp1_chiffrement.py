c312e45053ae7efb9652687c949a1ffdafea20ae
CaroPres6 <2043572@cegepmv.ca>
2025-09-29 22:18:33 -0400

    First commit

---
+++ laboratoires/TP1/src/tp1_chiffrement.py
@@ -0,0 +1,57 @@
+import random
+
+# create_permutation() a implementer: 
+def create_permutation():
+    alphabet = [chr(ord('a') + a) for a in range(26)]
+    alpha_shuffled = alphabet.copy()
+    random.shuffle(alpha_shuffled)
+    return dict(zip(alphabet, alpha_shuffled))
+
+permutation = create_permutation()
+
+text = "HelloWorldThisismyplaintext"
+shift = 3
+
+def encrypt(text,shift,permutation):
+    ciphertext = ""
+    for char in text:
+        if char.islower():
+            # faire le shift
+            shifted = chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
+            # faire la permu.
+            ciphertext += permutation[shifted]
+        elif char.isupper():
+            # faire le shift
+            shifted = chr((ord(char) - ord('A') + shift) % 26 + ord('A'))
+            shifted_lower = shifted.lower()
+            permuted = permutation[shifted_lower]
+            ciphertext += permuted.upper()
+            # si c'était en upper remettre upper
+        else:
+            # garder spaces, ponctu
+            ciphertext += char
+    return ciphertext
+
+
+
+""" cipher = encrypt(text, shift, permutation)
+print(cipher)
+plain = decrypt(cipher, shift, permutation)
+print(plain) """
+
+def decrypt(cipher, shift, permutation):
+    plaintext = ""
+    # Inverser permu.
+    permu_reverse = {v: k for k, v in permutation.items()}
+    for char in cipher:
+        if char.islower():
+            non_permuted = permu_reverse[char]
+        # Inverser shift
+            original = chr((ord(non_permuted) - ord('a') - shift) % 26 + ord('a'))
+            plaintext += original
+        elif char.isupper():
+            original = chr((ord(non_permuted) - ord('A') - shift) % 26 + ord('A'))
+            plaintext += original
+        else: 
+            plaintext += char
+    return plaintext
---
+++ laboratoires/TP1/src/tp2_passwords.py
@@ -0,0 +1,21 @@
+from ..utils.auth import *
+from ..utils.crypto_utils import *
+from ..data import *
+
+
+
+
+## avec salt impossible trouver password
+
+# mettre cette fonction dans crypto_utils quand fini et
+# faire appel fonction
+def look_each_psw():
+    users_db = load_csv('data/passwords_plain.csv')
+    # mettre les psw dans liste or smtg...
+    for usr in users_db:
+        username = usr['username']
+    for psw in list :
+        if check_login(username, psw, users_db, mode = 'password_only'):
+            print('Login réussi')
+        else:
+            print('Login échoué')
