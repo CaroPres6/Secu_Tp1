d0fbdb0c51ba24d9535a8bd27957f464ed39a3c3
CaroPres6 <2043572@cegepmv.ca>
2025-10-06 23:37:42 -0400

    Fonction pour loader les mots de passes dans un csv

--- laboratoires/TP1/src/tp2_passwords.py
+++ laboratoires/TP1/src/tp2_passwords.py
@@ -9,7 +9,7 @@ sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 
 
 from utils.auth import load_csv, check_login, hash_password
-from utils.crypto_utils import pt_1
+from utils.crypto_utils import pt_1, load_pw_from_csv
 
 # appel fonction pour la partie 1
 pt_1()
@@ -21,13 +21,16 @@ pt_1()
 
 # mettre cette fonction dans crypto_utils quand fini et
 # faire appel fonction
-""" def brute_force():
-    users_db = load_csv('data/passwords_plain.csv')
+def brute_force():
+    users_db = load_csv(os.path.join(os.path.dirname(__file__), '..', 'data', 'passwords_plain.csv'))
     # mettre les psw dans liste or smtg...
     for usr in users_db:
         username = usr['username']
-        for psw in list :
-            if check_login(username, psw, users_db, mode = 'password_only'):
+        for pwd in list :
+            if check_login(username, pwd, users_db, mode = 'password_only'):
                 print('Login réussi')
             else:
-                print('Login échoué') """
+                print('Login échoué') 
+
+# pour chq pw hash
+# compare pw myspace avec pw plain
